d07a02eb5f46014d8148fee2bc648d7d08090bbd
Atrio Technologies <122866434+Ayushzxz@users.noreply.github.com>
2024-04-19 18:32:42 +0530

    BOOKINGS

--- src/components/BookingData/BookingData.jsx
+++ src/components/BookingData/BookingData.jsx
@@ -1,37 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { ref, onValue } from 'firebase/database';
 import { database } from '../../firebase';
-import './BookingData.css'; 
+import './BookingData.css';
+import { Search } from '@mui/icons-material'; 
 
 const BookingData = () => {
     const [tableData, setTableData] = useState([]);
-    const [filterLocation, setFilterLocation] = useState('');
     const [filteredData, setFilteredData] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
-        const dbRef = ref(database);
-
-        onValue(dbRef, (snapshot) => {
-            const data = snapshot.val();
-            if (data) {
-                const userDetailsArray = Object.entries(data.UserDetails || {}).map(([key, value]) => ({
-                    id: key,
-                    ...value
-                }));
-                setTableData(userDetailsArray);
+        const fetchData = async () => {
+            const dbRef = ref(database);
+            
+            try {
+                onValue(dbRef, (snapshot) => {
+                    const data = snapshot.val();
+                    if (data) {
+                        const userDetailsArray = Object.values(data.UserDetails || {});
+                        const filteredData = userDetailsArray.filter(item => item.userLocation === 'KJC KOTHANUR');
+                        setTableData(filteredData);
+                        setFilteredData(filteredData); 
+                    } else {
+                        console.log("No data found.");
+                    }
+                }, (error) => {
+                    console.error("Error fetching data:", error);
+                });
+            } catch (error) {
+                console.error("Error fetching data:", error);
             }
-        });
+            }
+            fetchData();
     }, []);
 
-    useEffect(() => {
-        if (filterLocation !== '') {
-            const filtered = tableData.filter(user => user.userLocation === filterLocation);
-            setFilteredData(filtered);
-        } else {
-            setFilteredData(tableData);
-        }
-    }, [filterLocation, tableData]);
+    
 
     useEffect(() => {
         if (searchQuery !== '') {
@@ -42,11 +45,7 @@ const BookingData = () => {
         }
     }, [searchQuery, tableData]);
 
-    const getUniqueLocations = () => {
-        const uniqueLocations = new Set(); 
-        tableData.forEach(user => uniqueLocations.add(user.userLocation));
-        return Array.from(uniqueLocations);
-    };
+   
 
     const formatTime = (timeString) => {
         const timeParts = timeString.split(':');
@@ -74,62 +73,56 @@ const BookingData = () => {
     
     return (
         <div className="booking-data-container">
-            <h2 className="booking-data-title">Booking Data:</h2>
-            <div className="filter-container">
-                <label htmlFor="locationFilter">Filter by Location:</label>
-                <select id="locationFilter" value={filterLocation} onChange={(e) => setFilterLocation(e.target.value)}>
-                    <option value="">Select Location</option>
-                    {getUniqueLocations().map((location, index) => (
-                        <option key={index} value={location}>{location}</option>
-                    ))}
-                </select>
-                <div className="search-container">
-                    <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search by Name" />
-                    <button onClick={() => setSearchQuery('')}>
-                        Search
-                    </button>
-                </div>
+            <h2 className="booking-data-title">KJC KOTHANUR Bookings:</h2>
+            <div className="search-container">
+                <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search by Name" />
+                <button onClick={() => setSearchQuery('')}>
+                    <Search />
+                </button>
             </div>
-            <table className="booking-table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Address</th>
-                        <th>Location-Selected</th>
-                        <th>Phone No.</th>
-                        <th>Driving ID</th>
-                        <th>Selected Vehicle</th>
-                        <th>Vehicle Price</th>
-                        <th>Vehicle Category</th>
-                        <th>Pickup Date</th>
-                        <th>Drop-off Date</th>
-                        <th>Time</th>
-                        <th>Total Paid Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredData.map((userDetails, index) => (
-                        <tr key={userDetails.id}>
-                            <td>{index + 1}</td>
-                            <td>{userDetails.name}</td>
-                            <td>{userDetails.email}</td>
-                            <td>{userDetails.address}</td>
-                            <td>{userDetails.userLocation}</td>
-                            <td>{userDetails.tel}</td>
-                            <td>{userDetails.drivingID}</td>
-                            <td>{userDetails.vehicle_name}</td>
-                            <td>₹{userDetails.vehicle_price}</td>
-                            <td>{userDetails.vehicle_category}</td>
-                            <td>{formatDate(userDetails.pickUpDate)}</td>
-                            <td>{formatDate(userDetails.dropOffDate)}</td>
-                            <td>{formatTime(userDetails.time)}</td>
-                            <td>₹{userDetails.rentAmount}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div className="table-container">
+                <table className="booking-table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Address</th>
+                            {/* <th>Location-Selected</th> */}
+                            <th>Phone No.</th>
+                            <th>Driving ID</th>
+                            <th>Selected Vehicle</th>
+                            <th>Vehicle Price</th>
+                            <th>Vehicle Category</th>
+                            <th>Pickup Date</th>
+                            <th>Drop-off Date</th>
+                            <th>Time</th>
+                            <th>Total Paid Amount</th>
+
+                       </tr>
+                       </thead>
+                    <tbody>
+                        {filteredData.map((userDetails, index) => (
+                            <tr key={userDetails.id}>
+                                <td>{index + 1}</td>
+                                <td>{userDetails.name}</td>
+                                <td>{userDetails.email}</td>
+                                <td>{userDetails.address}</td>
+                                {/* <td>{userDetails.userLocation}</td> */}
+                                <td>{userDetails.tel}</td>
+                                <td>{userDetails.drivingID}</td>
+                                <td>{userDetails.vehicle_name}</td>
+                                <td>₹{userDetails.vehicle_price}</td>
+                                <td>{userDetails.vehicle_category}</td>
+                                <td>{formatDate(userDetails.pickUpDate)}</td>
+                                <td>{formatDate(userDetails.dropOffDate)}</td>
+                                <td>{formatTime(userDetails.time)}</td>
+                                <td>₹{userDetails.rentAmount}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
